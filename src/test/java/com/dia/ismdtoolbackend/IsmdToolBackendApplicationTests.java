41a265441329d27a2a869a0cc8e72b31d7fae527
Richard Koubek <koubek.richard@gmail.com>
2025-08-12 14:49:49 +0200

    project innit

---
+++ src/test/java/com/dia/ismdtoolbackend/IsmdToolBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dia.ismdtoolbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IsmdToolBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
