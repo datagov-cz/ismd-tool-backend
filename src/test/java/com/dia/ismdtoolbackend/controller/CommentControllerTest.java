c126908d2aff20a12f6c1f81f3c522555ebe4904
nicolas.bertelsen <stilas.bertelsen@gmail.com>
2026-03-30 08:39:57 +0200

    test: make tests independent of external auth

--- src/test/java/com/dia/ismdtoolbackend/controller/CommentControllerTest.java
+++ src/test/java/com/dia/ismdtoolbackend/controller/CommentControllerTest.java
@@ -12,10 +12,10 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Import;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.mockito.ArgumentMatchers.*;
@@ -35,7 +35,14 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
  * Note: @MockBean is deprecated in Spring Boot 3.4+ but remains the recommended
  * approach for @WebMvcTest until a clear migration path is provided.
  */
-@WebMvcTest(CommentController.class)
+@WebMvcTest(controllers = CommentController.class,
+    excludeAutoConfiguration = {
+        org.springframework.boot.autoconfigure.data.jpa.JpaRepositoriesAutoConfiguration.class,
+        org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration.class,
+        org.springframework.boot.autoconfigure.security.oauth2.client.OAuth2ClientAutoConfiguration.class,
+        org.springframework.boot.autoconfigure.security.oauth2.client.servlet.OAuth2ClientWebSecurityAutoConfiguration.class,
+        org.springframework.boot.autoconfigure.security.oauth2.resource.servlet.OAuth2ResourceServerAutoConfiguration.class
+    })
 @Import({TestSecurityConfig.class, TestOntologySecurityService.class, com.dia.ismdtoolbackend.config.GlobalExceptionHandler.class})
 @ActiveProfiles("test")
 class CommentControllerTest {
@@ -43,7 +50,7 @@ class CommentControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
-    @MockBean
+    @MockitoBean
     private CommentService commentService;
 
     @BeforeEach
