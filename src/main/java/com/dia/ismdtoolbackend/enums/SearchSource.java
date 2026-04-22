f758d8ae72b98205d050f6c645539a81224d726f
Richard Koubek <koubek.richard@gmail.com>
2026-04-22 11:38:47 +0200

    enum types for swagger generation

--- src/main/java/com/dia/ismdtoolbackend/enums/SearchSource.java
+++ src/main/java/com/dia/ismdtoolbackend/enums/SearchSource.java
@@ -2,7 +2,9 @@ package com.dia.ismdtoolbackend.enums;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonValue;
+import io.swagger.v3.oas.annotations.media.Schema;
 
+@Schema(type = "string", allowableValues = {"NKD", "ISMD", "ALL"})
 public enum SearchSource {
     NKD("NKD"),
     ISMD("ISMD"),
--- src/main/java/com/dia/ismdtoolbackend/enums/SearchType.java
+++ src/main/java/com/dia/ismdtoolbackend/enums/SearchType.java
@@ -2,7 +2,9 @@ package com.dia.ismdtoolbackend.enums;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonValue;
+import io.swagger.v3.oas.annotations.media.Schema;
 
+@Schema(type = "string", allowableValues = {"ONTOLOGY", "CONCEPT"})
 public enum SearchType {
     ONTOLOGY("ONTOLOGY"),
     CONCEPT("CONCEPT");
